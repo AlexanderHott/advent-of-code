41e5a5a3d8c33221c9be142c3f56ae528972da33
AlexanderHOtt <66271487+AlexanderHOtt@users.noreply.github.com>
2023-12-06 19:50:39 -0500

    day2 part2

--- 2023/rust/day2/src/main.rs
+++ 2023/rust/day2/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     let input = include_str!("../d2p1.txt");
     println!("{}", d2p1(input));
+    println!("{}", d2p2(input));
 }
 
 const RED: usize = 12;
@@ -36,15 +37,46 @@ fn d2p1(input: &str) -> usize {
     total
 }
 
+fn d2p2(input: &str) -> usize {
+    let mut total = 0;
+    for line in input.lines() {
+        let (_game, cubes) = line.split_once(": ").unwrap();
+        // let (_, game_id) = game.split_once(" ").unwrap();
+        // let game_id = game_id.parse::<usize>().unwrap();
+
+        let mut min_red = 0;
+        let mut min_green = 0;
+        let mut min_blue = 0;
+
+        for round in cubes.split("; ") {
+            for color in round.split(", ") {
+                match color.split_once(" ").unwrap() {
+                    (n, "red") => min_red = min_red.max(n.parse::<usize>().unwrap()),
+                    (n, "green") => min_green = min_green.max(n.parse::<usize>().unwrap()),
+                    (n, "blue") => min_blue = min_blue.max(n.parse::<usize>().unwrap()),
+                    _ => unreachable!(),
+                };
+            }
+        }
+
+        total += min_red * min_green * min_blue;
+    }
+
+    total
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
     #[test]
     fn test_pt1() {
-        let input = include_str!("../test_input1.txt");
+        let input = include_str!("../test_input.txt");
         assert_eq!(8, d2p1(input));
     }
 
     #[test]
-    fn test_pt2() {}
+    fn test_pt2() {
+        let input = include_str!("../test_input.txt");
+        assert_eq!(2286, d2p2(input));
+    }
 }
